83d18dcfff39750013d8cca8f16db6704e4519da
Armlockx <reus_julio@hotmail.com>
2024-03-25 12:50:31 -0300

    Add initial Author search

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection
@@ -21,5 +24,6 @@ db.once('open', () => console.log('Connected to Mongoose.'));
 
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
